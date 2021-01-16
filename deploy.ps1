f4ecec440c8222051f06379105786a53ad06f094
Gabriel Cascaes <gabriel.cascaes@harbor.com.br>
2021-01-16 16:26:34 -0300

    Updated deploy script to use version from info.json and also create release zip file.

--- deploy.ps1
+++ deploy.ps1
@@ -1,2 +1,6 @@
-Remove-Item -path "$env:APPDATA\Factorio\mods\Filter_Combinator_1.1.1\*" -Force -Recurse
-Copy-Item -Path "*" -Destination "$env:APPDATA\Factorio\mods\Filter_Combinator_1.1.1" -PassThru -Force -Recurse -Exclude @(".vs", ".gitignore", "deploy.ps1", "README.md", ".git")
+$version = ((Get-Content "info.json" -Raw) | ConvertFrom-Json).version
+
+Remove-Item -path "$env:APPDATA\Factorio\mods\Filter_Combinator_$version\*" -Force -Recurse
+Copy-Item -Path "*" -Destination "$env:APPDATA\Factorio\mods\Filter_Combinator_$version" -PassThru -Force -Recurse -Exclude @(".vs", ".gitignore", "deploy.ps1", "README.md", ".git")
+
+Compress-Archive -LiteralPath "$env:APPDATA\Factorio\mods\Filter_Combinator_$version" -DestinationPath ".\Releases\Filter_Combinator_$version.zip" -Update
