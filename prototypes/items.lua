d3d71f8c1db087bc65bff9e90a48b54725fde741
Gabriel Cascaes <gabriel.cascaes@harbor.com.br>
2021-01-16 16:26:00 -0300

    Added filter combinator icon.

--- prototypes/items.lua
+++ prototypes/items.lua
@@ -3,5 +3,6 @@ local combinator = table.deepcopy(data.raw["item"]["arithmetic-combinator"])
 combinator.name = "signal-filter-combinator"
 combinator.place_result="signal-filter-combinator"
 combinator.order = "b[combinators]-d[signal-filter-combinator]"
+combinator.icon = "__Filter_Combinator__/graphics/icons/filter-combinator.png"
 
 data:extend{combinator}
