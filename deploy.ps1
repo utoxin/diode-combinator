0976cced1c77d8f9314a513512aab138657d1c37
Gabriel Cascaes <gabriel.cascaes@harbor.com.br>
2021-01-16 18:56:26 -0300

    Updated deploy script to use 7zip.

--- deploy.ps1
+++ deploy.ps1
@@ -3,4 +3,15 @@ $version = ((Get-Content "info.json" -Raw) | ConvertFrom-Json).version
 Remove-Item -path "$env:APPDATA\Factorio\mods\Filter_Combinator_$version\*" -Force -Recurse
 Copy-Item -Path "*" -Destination "$env:APPDATA\Factorio\mods\Filter_Combinator_$version" -PassThru -Force -Recurse -Exclude @(".vs", ".gitignore", "deploy.ps1", "README.md", ".git")
 
-Compress-Archive -LiteralPath "$env:APPDATA\Factorio\mods\Filter_Combinator_$version" -DestinationPath ".\Releases\Filter_Combinator_$version.zip" -Update
+$7zipPath = "$env:ProgramFiles\7-Zip\7z.exe"
+
+if (-not (Test-Path -Path $7zipPath -PathType Leaf)) {
+    throw "7 zip file '$7zipPath' not found"
+}
+
+Set-Alias 7zip $7zipPath
+
+$Source = "c:\BackupFrom\backMeUp.txt"
+$Target = "c:\BackupFolder\backup.zip"
+
+7zip a -mx=9 ".\Releases\Filter_Combinator_$version.zip" "$env:APPDATA\Factorio\mods\Filter_Combinator_$version"
